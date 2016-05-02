306f2c698554b98ae236a1a4a2eb6be757f4cc31
rostard <kos.boykow@gmail.com>
2016-05-02 21:53:38 +0300

    Fix mesh

--- OpenGL/Tutorial/mesh.cpp
+++ OpenGL/Tutorial/mesh.cpp
@@ -133,7 +133,7 @@ bool Mesh::InitFromScene(const aiScene* pScene, const std::string& Filename)
 	glBindBuffer(GL_ARRAY_BUFFER, m_Buffers[NORMAL_VB]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Normals[0])*Normals.size(), &Normals[0], GL_STATIC_DRAW);
 	glEnableVertexAttribArray(2);
-	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_Buffers[INDEX_BUFFER]);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices[0])*Indices.size(), &Indices[0], GL_STATIC_DRAW);
@@ -233,10 +233,10 @@ void Mesh::Render()
 		assert(MaterialIndex < m_Textures.size());
 
 		if (m_Textures[MaterialIndex]){
-			m_Textures[MaterialIndex]->Bind(GL_TEXTURE0);
+			m_Textures[MaterialIndex]->Bind(COLOR_TEXTURE_UNIT);
 		}
 
-		glDrawElementsBaseVertex(GL_TRIANGLES,
+		glDrawElementsBaseVertex(GL_PATCHES,
 								m_Entries[i].NumIndices,
 								GL_UNSIGNED_INT,
 								(void*)(sizeof(unsigned int)*m_Entries[i].BaseIndex),
